ccc9bc93d4fe52121e8a5e6cde3fee6f58afe3fe
josojo <josojo@hotmail.de>
2021-09-01 12:58:41 +0200

    docker deploy setup

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_NAME" --password-stdin
 # Build and tag api-image
-docker build -t "${DOCKERHUB_PROJECT}-api" -f docker/Dockerfile.binary .
-docker tag "${DOCKERHUB_PROJECT}-api" gnosispm/"${DOCKERHUB_PROJECT}-api":$1
-docker push gnosispm/"${DOCKERHUB_PROJECT}-api":$1
+docker build -t "${DOCKERHUB_PROJECT}" -f docker/Dockerfile.binary .
+docker tag "${DOCKERHUB_PROJECT}" gnosispm/"${DOCKERHUB_PROJECT}":$1
+docker push gnosispm/"${DOCKERHUB_PROJECT}":$1
 
 # Build and tag dune-data-fetcher-image
 cd dune_data_api
