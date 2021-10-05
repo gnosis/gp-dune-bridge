223bd8075d761413671ca83d761beeb90719c4c3
josojo <josojo@hotmail.de>
2021-10-05 11:30:28 +0200

    factoring out the functions into reusable utils.py

--- dune_api_scripts/utils.py
+++ dune_api_scripts/utils.py
@@ -1,8 +1,8 @@
-from datetime import datetime
-from pathlib import Path
-from duneanalytics import DuneAnalytics
-import os
 import json
+import os
+from duneanalytics import DuneAnalytics
+from pathlib import Path
+from datetime import datetime
 
 
 def dune_from_environment():
@@ -18,16 +18,15 @@ def parse_data_from_dune_query(data):
     user_data = data["data"]["get_result_by_result_id"]
     date_of_data_creation = datetime.strptime(
         user_data[0]["data"]["day"][0:10], '%Y-%m-%d')
-    return {
-        "user_data": user_data,
-        "time_of_download": date_of_data_creation.strftime("%d/%m/%Y %H:%M:%S")
-    }
+    return {"user_data": user_data,
+            "time_of_download": date_of_data_creation.strftime("%d/%m/%Y %H:%M:%S")}
 
 
 def store_as_json_file(data_set):
-    file_path = Path(os.environ['DUNE_DATA_FOLDER'] + "/user_data/")
+    file_path = Path(os.environ['DUNE_DATA_FOLDER'] +
+                     "/user_data/")
     os.makedirs(file_path,  exist_ok=True)
-    file_name = Path("user_data_from{}.json".format(datetime.now()))
-    with open(os.path.join(file_path, file_name), 'w+', encoding='utf-8') as f:
+    with open(os.path.join(file_path, Path("user_data_from" + datetime.now().strftime("%m-%d-%Y") + ".json")), 'w+', encoding='utf-8') as f:
         json.dump(data_set, f, ensure_ascii=False, indent=4)
-    print("Written updates into: " + os.path.join(file_path, file_name))
+    print("Written updates into: " + os.path.join(file_path,
+          Path("user_data_from" + datetime.now().strftime("%m-%d-%Y") + ".json")))
