f7f510b9610af56704ac1bf16a281668cff4f52b
josojo <josojo@hotmail.de>
2021-10-07 11:27:55 +0200

    Ben's finding of false merges

--- dune_api_scripts/utils.py
+++ dune_api_scripts/utils.py
@@ -18,17 +18,19 @@ def parse_data_from_dune_query(data):
     user_data = data["data"]["get_result_by_result_id"]
     date_of_data_creation = datetime.strptime(
         user_data[0]["data"]["day"][0:10], '%Y-%m-%d')
-    return {"user_data": user_data,
-            "time_of_download": date_of_data_creation.strftime("%d/%m/%Y %H:%M:%S")}
+    return {
+        "user_data": user_data,
+        "time_of_download": date_of_data_creation.strftime("%d/%m/%Y %H:%M:%S")
+    }
 
 
 def store_as_json_file(data_set):
     file_path = Path(os.environ['DUNE_DATA_FOLDER'] + "/user_data/")
     os.makedirs(file_path,  exist_ok=True)
-    with open(os.path.join(file_path, Path("user_data_from" + datetime.now().strftime("%m-%d-%Y") + ".json")), 'w+', encoding='utf-8') as f:
+    file_name = Path("user_data_from{}.json".format(datetime.now()))
+    with open(os.path.join(file_path, file_name), 'w+', encoding='utf-8') as f:
         json.dump(data_set, f, ensure_ascii=False, indent=4)
-    print("Written updates into: " + os.path.join(file_path,
-          Path("user_data_from" + datetime.now().strftime("%m-%d-%Y") + ".json")))
+    print("Written updates into: " + os.path.join(file_path, file_name))
 
 
 def build_string_for_affiliate_referrals_pairs():
