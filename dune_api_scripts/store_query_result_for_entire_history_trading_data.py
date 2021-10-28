0d00533d8c5fa5d114c2b772b528a9ba286371d6
Ben Smith <bh2smith@gmail.com>
2021-10-28 10:48:41 +0200

    use if name == main

--- dune_api_scripts/store_query_result_for_entire_history_trading_data.py
+++ dune_api_scripts/store_query_result_for_entire_history_trading_data.py
@@ -8,30 +8,31 @@ import os
 from .utils import parse_data_from_dune_query, open_downloaded_history_file, \
     ensure_that_download_is_recent, dune_from_environment
 
-# Entire history does not need to be downloaded again,
-# if file was already downloaded in the past and exists.
-file_entire_history = open_downloaded_history_file()
+if __name__ == "__main__":
+    # Entire history does not need to be downloaded again,
+    # if file was already downloaded in the past and exists.
+    file_entire_history = open_downloaded_history_file()
 
-# initialize the environment
-dune = dune_from_environment()
+    # initialize the environment
+    dune = dune_from_environment()
 
-# fetch query result id using query id
-query_id = int(os.getenv('QUERY_ID_ENTIRE_HISTORY_TRADING_DATA', "157348"))
-result_id = dune.query_result_id(query_id)
+    # fetch query result id using query id
+    query_id = int(os.getenv('QUERY_ID_ENTIRE_HISTORY_TRADING_DATA', "157348"))
+    result_id = dune.query_result_id(query_id)
 
-# fetch query result
-data = dune.query_result(result_id)
+    # fetch query result
+    data = dune.query_result(result_id)
 
-# parse data
-data_set = parse_data_from_dune_query(data)
+    # parse data
+    data_set = parse_data_from_dune_query(data)
 
-# in case the data is not from within the last 30 mins,
-# we want to wait for a new query result and hence exit:
-ensure_that_download_is_recent(data_set["time_of_download"], 30 * 60)
-ensure_that_download_is_recent("HELLO", 1)
-# write data to file, if non-empty
-if bool(data_set):
-    with open(file_entire_history, 'w+', encoding='utf-8') as f:
-        json.dump(data_set, f, ensure_ascii=False, indent=4)
-else:
-    print("query is still calculating")
+    # in case the data is not from within the last 30 mins,
+    # we want to wait for a new query result and hence exit:
+    ensure_that_download_is_recent(data_set["time_of_download"], 30 * 60)
+
+    # write data to file, if non-empty
+    if bool(data_set):
+        with open(file_entire_history, 'w+', encoding='utf-8') as f:
+            json.dump(data_set, f, ensure_ascii=False, indent=4)
+    else:
+        print("query is still calculating")
